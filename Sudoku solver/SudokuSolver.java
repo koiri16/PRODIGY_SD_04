a7376a09036b7d83b09f43d8440e14cefdfcab5c
koiri16 <120775850+koiri16@users.noreply.github.com>
2024-05-18 14:35:57 +0530

    Add files via upload

---
+++ Sudoku solver/SudokuSolver.java
@@ -0,0 +1,89 @@
+public class SudokuSolver {
+    private static final int SIZE = 9; // Size of the Sudoku grid
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    // Method to solve Sudoku using backtracking
+    private static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                // Find an empty cell
+                if (board[row][col] == 0) {
+                    // Try all possible numbers
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            // Recursively attempt to fill in the rest of the board
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            // Backtrack if solution not found
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false; // No valid number found, trigger backtracking
+                }
+            }
+        }
+        return true; // Board is complete
+    }
+
+    // Method to check if it's valid to place num at board[row][col]
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check row
+        for (int x = 0; x < SIZE; x++) {
+            if (board[row][x] == num) {
+                return false;
+            }
+        }
+        // Check column
+        for (int x = 0; x < SIZE; x++) {
+            if (board[x][col] == num) {
+                return false;
+            }
+        }
+        // Check 3x3 subgrid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Method to print the Sudoku board
+    private static void printBoard(int[][] board) {
+        for (int r = 0; r < SIZE; r++) {
+            for (int d = 0; d < SIZE; d++) {
+                System.out.print(board[r][d]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+            if ((r + 1) % 3 == 0) {
+                System.out.print("");
+            }
+        }
+    }
+}
